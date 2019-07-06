5df902cca70618876fc50ef9f8279c294774f04f
xiaoquanquanxin <xxkl123@126.com>
2019-07-06 21:19:13 +0800

    加入了postcss

--- src/web/components/userBehavior/subRoute.tsx
+++ src/web/components/userBehavior/subRoute.tsx
@@ -1,20 +1,17 @@
 import * as React from "react";
-import {
-    HashRouter as Router,
-    Link, Route
-} from 'react-router-dom'
+//  @ts-ignore
+import {Link, NavLink} from 'react-router-dom'
 
-function SubRoute({rootRoute}) {
+interface RootRoute {
+    rootRoute: string
+}
+
+function SubRoute(params: RootRoute) {
+    console.log(params.rootRoute)
     return (
         <ul className={'clear'}>
-            <li>
-                <Link to={rootRoute + ''}>普通</Link>
-                {/*<a href={rootRoute + ''}>普通</a>*/}
-            </li>
-            <li>
-                <a href={rootRoute + '/abc'}>abc</a>
-                {/*<Link to={rootRoute + '/abc'}>abc</Link>*/}
-            </li>
+            <li><NavLink to={params.rootRoute}>行为的首页,自路由不能用啊</NavLink></li>
+            <li><NavLink to={params.rootRoute + '/abc'}>abc</NavLink></li>
         </ul>
     );
 }
