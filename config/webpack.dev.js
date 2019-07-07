03bea7322a677094f8cbb0bd9cc3b7482cfe4d6f
quanxin <3059210183@qq.com>
2019-07-07 09:52:17 +0800

    webpack配置文件修改

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,12 @@
 const merge = require('webpack-merge');
 const webpackCommon = require('./webpack.common.js');
+
+
+
+console.log('webpack.dev.js     //loading ........................................');
+console.log('webpack.dev.js     //loading ........................................');
+console.log('webpack.dev.js     //loading ........................................');
+console.log('webpack.dev.js     //loading ........................................');
 const webpackDevConfiguration = merge(webpackCommon, {
     mode: 'development',
     devtool: 'inline-source-map',
@@ -7,6 +14,4 @@ const webpackDevConfiguration = merge(webpackCommon, {
         contentBase: './dist'
     }
 });
-console.log('webpack.dev.js     //loading ........................................');
-// console.log(webpackDevConfiguration);
 module.exports = webpackDevConfiguration;
